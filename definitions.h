78309c2beea0d1cbbda61a4eb5ddc6e6a89c32dc
Yicheng Hu <75612148+Jellal-HT@users.noreply.github.com>
2021-03-05 23:02:12 -0500

    Create definitions.h

---
+++ definitions.h
@@ -0,0 +1,7 @@
+#ifndef DEFINITIONS_H_INCLUDED
+#define DEFINITIONS_H_INCLUDED
+	void InitMatrix(int layer, int row, int column, int a[layer][row][column]);
+	void PrintMatrix(int layer, int row, int column, int a[layer][row][column]);
+	void SortMatrix(int* first, int sum);
+	void PrintMatrixLinear(int layer, int row, int column, int a[layer][row][column]);
+#endif // DEFINITIONS_H_INCLUDED
